d804f1b56c4f64caab7a8b15466a6eacb43e6f5b
xent <alexdmitv@gmail.com>
2017-12-31 22:44:49 +0300

    Added generic semaphore for bare-metal systems.

--- include/osw/generic/semaphore.h
+++ include/osw/generic/semaphore.h
@@ -1,62 +1,61 @@
 /*
- * osw/linux/semaphore.h
+ * osw/generic/semaphore.h
  * Copyright (C) 2017 xent
  * Project is distributed under the terms of the GNU General Public License v3.0
  */
 
-#ifndef OSW_LINUX_SEMAPHORE_H_
-#define OSW_LINUX_SEMAPHORE_H_
+#ifndef OSW_GENERIC_SEMAPHORE_H_
+#define OSW_GENERIC_SEMAPHORE_H_
 /*----------------------------------------------------------------------------*/
-#include <assert.h>
-#include <semaphore.h>
 #include <stdbool.h>
 #include <xcore/helpers.h>
 #include <xcore/error.h>
 /*----------------------------------------------------------------------------*/
 struct Semaphore
 {
-  sem_t handle;
+  int value;
 };
-/*----------------------------------------------------------------------------*/
-BEGIN_DECLS
-
-bool semTryWait(struct Semaphore *, unsigned int);
-
-END_DECLS
 /*----------------------------------------------------------------------------*/
 BEGIN_DECLS
 
 static inline enum Result semInit(struct Semaphore *sem, int value)
 {
-  return sem_init(&sem->handle, 0, value) == 0 ? E_OK : E_ERROR;
+  sem->value = value;
+  return E_OK;
 }
 
-static inline void semDeinit(struct Semaphore *sem)
+static inline void semDeinit(struct Semaphore *sem
+    __attribute__((unused)))
 {
-  sem_destroy(&sem->handle);
 }
 
 static inline void semPost(struct Semaphore *sem)
 {
-  sem_post(&sem->handle);
+  __atomic_fetch_add(&sem->value, 1, __ATOMIC_SEQ_CST);
+}
+
+static inline bool semTryWait(struct Semaphore *sem __attribute__((unused)),
+    unsigned int value __attribute__((unused)))
+{
+  while (1); /* TODO Unimplemented */
+
+  return false;
 }
 
 static inline int semValue(struct Semaphore *sem)
 {
   int value;
 
-  const int result = sem_getvalue(&sem->handle, &value);
-  assert(result == 0);
-  (void)result;
-
+  __atomic_load(&sem->value, &value, __ATOMIC_SEQ_CST);
   return value;
 }
 
 static inline void semWait(struct Semaphore *sem)
 {
-  sem_wait(&sem->handle);
+  while (semValue(sem) <= 0);
+  __atomic_fetch_sub(&sem->value, 1, __ATOMIC_SEQ_CST);
 }
 
 END_DECLS
 /*----------------------------------------------------------------------------*/
-#endif /* OSW_LINUX_SEMAPHORE_H_ */
+#endif /* OSW_GENERIC_SEMAPHORE_H_ */
---
+++ sources/generic/semaphore.c
@@ -0,0 +1,7 @@
+/*
+ * semaphore.c
+ * Copyright (C) 2014 xent
+ * Project is distributed under the terms of the GNU General Public License v3.0
+ */
+
+#include <osw/semaphore.h>
